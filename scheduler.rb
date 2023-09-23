4cb7a3f0bc44131b46ef3c9aa7470e5d02ba08b9
Andrii Aheiev <a.ageyev@gmail.com>
2023-09-23 18:50:39 +0200

    add cleanup job to the scheduler

--- scheduler.rb
+++ scheduler.rb
@@ -3,6 +3,8 @@ include Clockwork
 
 handler do |job|
   case job
+  when 'cleanup_ubibot_auth'
+    %x[jets runner "UbibotAuth::cleanup"]
   when 'update_sp1'
     %x[jets runner "UpdateChannelsJob.perform_now(:sp1)"]
   when 'update_ws1'
@@ -14,3 +16,4 @@ end
 
 every(1.minute,  'update_sp1')
 every(5.minutes,  'update_ws1')
+every(4.hours,  'cleanup_ubibot_auth')
